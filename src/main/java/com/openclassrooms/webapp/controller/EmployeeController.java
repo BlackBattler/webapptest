73edb71e203bcdc58dcfdf8d4ebe8509f5d2afbb
Battler <battler.business@gmail.com>
2025-07-11 13:50:50 +0200

    correction bug updateemployee

--- src/main/java/com/openclassrooms/webapp/controller/EmployeeController.java
+++ src/main/java/com/openclassrooms/webapp/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.openclassrooms.webapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.openclassrooms.webapp.model.Employee;
